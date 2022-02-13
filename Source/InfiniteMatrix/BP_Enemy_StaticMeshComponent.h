f596fc3e314766636b4042b58ea55b77f4afe4f4
abhisathya4 <asathya2@illinois.edu>
2022-02-13 17:41:02 -0600

    CS498 InfiniteMatrix

---
+++ Source/InfiniteMatrix/BP_Enemy_StaticMeshComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "BP_Enemy_StaticMeshComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INFINITEMATRIX_API UBP_Enemy_StaticMeshComponent : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/InfiniteMatrix/ProjectileStaticMesh.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "ProjectileStaticMesh.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INFINITEMATRIX_API UProjectileStaticMesh : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+};
